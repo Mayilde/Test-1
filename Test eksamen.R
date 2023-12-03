862ac5904d0ddd01139b29e46f9acd4a0c208aab
[Mayilde] <matildec2000@gmail.com>
2023-12-03 14:17:02 +0000

    conlfict

--- Test eksamen.R
+++ Test eksamen.R
@@ -1,9 +1,3 @@
 library(ggplot2) #Can be used to several graphs
-fejl
-hall
-l
-hello
-nej! slfnsådan her
-sådan heeeeer 
-#uenig - hvad mener du? hva.
-lav nu en conflict...
+Lav en conflict, tak. 
+
